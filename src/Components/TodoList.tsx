7b3f24d2e1ecf0db50bd3c0017e5230c136b5313
THEMOMMA <themomma@THEMOMMAui-MacBookPro.local>
2021-09-24 14:41:42 +0900

    TodoList Context 상태 조회

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -1,35 +1,10 @@
 import styled from 'styled-components';
 
 import TodoItem from './TodoItem';
+import { useTodosState } from 'contexts/TodosContext';
 
 const TodoList = () => {
-  const todos = [
-    {
-      id: 1,
-      text: 'TypeScript',
-      done: true,
-    },
-    {
-      id: 2,
-      text: 'Context API',
-      done: true,
-    },
-    {
-      id: 3,
-      text: 'React',
-      done: false,
-    },
-    {
-      id: 4,
-      text: 'JavaScript',
-      done: false,
-    },
-    {
-      id: 5,
-      text: 'Redux',
-      done: true,
-    },
-  ];
+  const todos = useTodosState();
 
   return (
     <TodoListWrapper>
